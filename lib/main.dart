52170af0d12f6c5129d974ff1ce11418fd7bf072
tzupingchenwork <andychentzupingwork@gmail.com>
2024-01-05 15:29:56 +0800

    Refactor home page and add TOTP key functionality

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Authenticator Home Page'),
+      home: MyHomePage(title: 'Authenticator Home Page'),
       routes: {
         '/scan': (context) => const ScanPage(),
         '/list': (context) => const TotpListPage(),
@@ -35,7 +35,9 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
+
+  final TextEditingController controller = TextEditingController();
 
   @override
   // ignore: library_private_types_in_public_api
@@ -63,6 +65,56 @@ class _MyHomePageState extends State<MyHomePage> {
                   );
                 },
               ),
+              ElevatedButton(
+                child: const Text('Add TOTP Key'),
+                onPressed: () {
+                  // show a dialog to add a new key
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Add TOTP Key'),
+                        content: TextField(
+                          controller: widget.controller,
+                          autofocus: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Key',
+                          ),
+                          onSubmitted: (String value) {
+                            Provider.of<TotpProvider>(context, listen: false)
+                                .addCode(value);
+                            Navigator.pop(context);
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              String value = widget.controller.text;
+                              Provider.of<TotpProvider>(context, listen: false)
+                                  .addCode(value);
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Add'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Provider.of<TotpProvider>(context, listen: false)
+                      .removeAllCodes();
+                },
+                child: const Text('Clear All'),
+              )
             ],
           ),
         ),
