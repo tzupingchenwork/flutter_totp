00cc03166ddd05165eacd7ac72f9c0072840d3b1
tzupingchenwork <andychentzupingwork@gmail.com>
2024-01-05 15:30:41 +0800

    Add time synchronization with server and code management functionality

--- lib/totp_provider.dart
+++ lib/totp_provider.dart
@@ -1,24 +1,75 @@
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:otp/otp.dart';
+import 'dart:convert';
+import 'package:ntp/ntp.dart';
 
 class TotpProvider with ChangeNotifier {
   final FlutterSecureStorage storage = const FlutterSecureStorage();
   List<String> codes = [];
+  Timer? timer;
 
   TotpProvider() {
     loadCodes();
+    timer =
+        Timer.periodic(const Duration(seconds: 30), (Timer t) => loadCodes());
+  }
+
+  int _timeOffset = 0;
+
+  Future<void> syncTimeWithServer() async {
+    try {
+      final DateTime ntpTime = await NTP.now();
+      _timeOffset = ntpTime.millisecondsSinceEpoch -
+          DateTime.now().millisecondsSinceEpoch;
+    } catch (e) {
+      print(e);
+    }
   }
 
   Future<void> loadCodes() async {
     final allKeys = await storage.readAll();
     if (allKeys.isNotEmpty) {
-      codes = allKeys.values.map((key) {
-        return OTP.generateTOTPCodeString(
-            key, DateTime.now().millisecondsSinceEpoch,
-            interval: 30, algorithm: Algorithm.SHA1, length: 6);
-      }).toList();
+      final currentTime = DateTime.now().millisecondsSinceEpoch + _timeOffset;
+      codes = List<String>.from(allKeys.values.map((jsonStr) {
+        if (jsonStr.startsWith('{')) {
+          var map = jsonDecode(jsonStr);
+          var secret = map['secret'];
+          if (secret != null) {
+            return OTP.generateTOTPCodeString(secret, currentTime,
+                interval: 30,
+                algorithm: Algorithm.SHA1,
+                length: 6,
+                isGoogle: true);
+          }
+        }
+      }).where((code) => code != null));
       notifyListeners();
     }
+  }
+
+  Future<void> addCode(String key) async {
+    await storage.write(key: key, value: key);
+    await loadCodes();
+    notifyListeners();
+  }
+
+  Future<void> removeCode(String key) async {
+    await storage.delete(key: key);
+    await loadCodes();
+    notifyListeners();
+  }
+
+  Future<void> removeAllCodes() async {
+    await storage.deleteAll();
+    await loadCodes();
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 }
