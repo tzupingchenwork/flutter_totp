17015fa56b6c0d04721d0140815ccc18c392f969
tzupingchenwork <andychentzupingwork@gmail.com>
2024-01-05 15:31:05 +0800

    Add parsing and storing of OTPAUTH URI

--- lib/scan.page.dart
+++ lib/scan.page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -32,15 +33,27 @@ class _ScanPageState extends State<ScanPage> {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) async {
       controller.pauseCamera();
+
+      // Parse the OTPAUTH URI
+      var uri = Uri.parse(scanData.code!);
+      var secret = uri.queryParameters['secret'];
+      var issuer = uri.queryParameters['issuer'];
+
+      // Store the secret and issuer
       String key = 'totpKey_${DateTime.now().millisecondsSinceEpoch}';
-      await widget.storage.write(key: key, value: scanData.code);
+      String value = jsonEncode({'secret': secret, 'issuer': issuer});
+      await widget.storage.write(key: key, value: value);
+
+      // Show a snack bar
       // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('QR Code scanned successfully! ${scanData.code}}'),
+          content: Text(
+              'QR Code scanned successfully! Secret: $secret, Issuer: $issuer'),
         ),
       );
-      // 等待一秒後返回 list page
+
+      // Wait for one second and then navigate to the list page
       Future.delayed(const Duration(seconds: 1)).then((_) {
         Navigator.pushNamed(context, '/list');
       });
